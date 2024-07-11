4dd481bd2c94a90436dcff5a354eccb5b4036b19
mrxzeta <oscau78@gmail.com>
2024-07-11 09:11:12 -0600

    new

--- ProyectoIntegrador/Contacto/registro.php
+++ ProyectoIntegrador/Contacto/registro.php
@@ -11,7 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("La conexión falló: " . $conn->connect_error);
 }
-echo "Conexión exitosa";
 ?>
 
 <?php
